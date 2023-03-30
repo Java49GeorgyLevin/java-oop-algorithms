ca1acf15aef7730c9d009f1d402daac1813ea6b1
Georgy Levin <g.phyzy@gmail.com>
2023-03-30 23:37:58 +0300

    HW#3 Collection, List, ArrayList initial

---
+++ src/telran/util/ArrayList.java
@@ -0,0 +1,61 @@
+package telran.util;
+
+import java.util.Arrays;
+
+public class ArrayList<T> implements List<T> {
+	private static final int DEFAULT_CAPACITY = 16;
+	private T[] array;
+	private int size;	
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+		array = (T[]) new Object[capacity];
+	}
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
+		
+	}
+
+	@Override
+	public boolean add(T obj) {
+		if(size == array.length) {
+			reallocate();
+		}
+		array[size] = obj;
+		size++;
+		return true;
+	}
+	
+	private void reallocate() {
+		array = Arrays.copyOf(array, array.length * 2);
+	}
+
+	@Override
+	public void add(int index, T obj) {
+		if(size == array.length) {
+			reallocate();
+		}
+		System.arraycopy(array, index, array, index+1, array.length-index-1);
+		array[index] = obj;
+		size++;
+	}
+
+	@Override
+	public T remove(int index) {
+		T removed = array[index];
+		System.arraycopy(array, index+1, array, index, array.length-index-1);
+		array = Arrays.copyOf(array, array.length - 1);
+		size--;
+		return removed;
+	}
+
+	@Override
+	public T get(int index) {
+		return array[index];
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+}
---
+++ src/telran/util/test/ArrayListTest.java
@@ -0,0 +1,63 @@
+package telran.util.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayListTest {
+
+	@Test
+	void testAdd() {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		ArrayList<String> strings = new ArrayList<>();
+		numbers.add(5);
+		numbers.add(10);
+		strings.add("ABC");
+		strings.add("DEF");
+		assertEquals(2, numbers.size());
+		assertEquals(2, strings.size());
+	}
+	
+	@Test
+		void testAddIndex() {
+		telran.util.ArrayList<Integer> num =  new telran.util.ArrayList<>();
+		for(int i = 0; i < 5; i++) {
+		num.add(i * 2);
+		}
+		assertEquals(5, num.size());
+		num.add(5, 5);
+		assertEquals(5, num.get(5));
+		assertEquals(6, num.size());
+		num.add(3, 3);
+		assertEquals(3, num.get(3));
+		assertEquals(7, num.size());
+		num.add(0, 0);
+		assertEquals(8, num.size());
+		assertEquals(0, num.get(0));
+		assertEquals(5, num.get(7));
+		assertEquals(3, num.get(4));		
+	}
+	
+	@Test
+		void testRemoveIndex() {
+		telran.util.ArrayList<Integer> num = new telran.util.ArrayList<>();
+		for(int i = 0; i < 5; i++) {
+		num.add(i * 3);
+		}
+		assertEquals(12, num.get(4));
+		assertEquals(5, num.size());
+		assertEquals(12, num.remove(4));
+		assertEquals(6, num.get(2));
+		assertEquals(4, num.size());
+		assertEquals(6, num.remove(2));
+		assertEquals(0, num.get(0));
+		assertEquals(3, num.size());
+		assertEquals(0, num.remove(0));
+		assertEquals(2, num.size());
+
+
+		
+	}
+}
