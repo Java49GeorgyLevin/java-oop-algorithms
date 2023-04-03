9da990a3d1871c3b22fd2dd65047349ba8157a6a
Georgy Levin <g.phyzy@gmail.com>
2023-04-03 18:40:12 +0300

    CW4

--- src/telran/util/ArrayList.java
+++ src/telran/util/ArrayList.java
@@ -12,9 +12,7 @@ public class ArrayList<T> implements List<T> {
 	}
 	public ArrayList() {
 		this(DEFAULT_CAPACITY);
-		
 	}
-
 	@Override
 	public boolean add(T obj) {
 		if(size == array.length) {
@@ -24,44 +22,73 @@ public class ArrayList<T> implements List<T> {
 		size++;
 		return true;
 	}
-	
+
 	private void reallocate() {
 		array = Arrays.copyOf(array, array.length * 2);
+		
 	}
-
 	@Override
 	public void add(int index, T obj) {
-		if(size == array.length) {
+		if (size == array.length) {
 			reallocate();
 		}
-		System.arraycopy(array, index, array, index+1, size-index);
+		System.arraycopy(array, index, array, index + 1, size - index);
 		array[index] = obj;
 		size++;
 	}
 
 	@Override
 	public T remove(int index) {
-		T removed = array[index];
-System.out.println(index);
-System.out.println("in" + Arrays.toString(array));
-	//System.arraycopy(array, index+1, array, index, size-index-1);
-		System.arraycopy(array, index+1, array, index, size-index);
-//System.out.println(Arrays.toString(array));
-	//array = Arrays.copyOf(array, size-1);
-	//array = Arrays.copyOf(array, size-1);
-System.out.println("out" + Arrays.toString(array));
+		T res = array[index];
+		
+		System.arraycopy(array, index + 1, array, index,
+				size - index - 1);
 		size--;
-		return removed;
+		return res;
 	}
 
 	@Override
 	public T get(int index) {
-		return array[index];
+		T res = array[index];
+		return res;
 	}
-
 	@Override
 	public int size() {
+		
 		return size;
 	}
+	@Override
+	public boolean remove(T pattern) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public T[] toArray(T[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int indexOf(T pattern) {
+		int res = -1;
+		int index = 0;
+		while(index < size && res == -1) {
+			if (isEqual(array[index], pattern)) {
+				res = index;
+			}
+			index++;
+		}
+		return res;
+	}
+	private boolean isEqual(T object, T pattern) {
+		
+		return pattern == null ? object == pattern :
+			pattern.equals(object);
+	}
+	@Override
+	public int lastIndexOf(T pattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 
 }
