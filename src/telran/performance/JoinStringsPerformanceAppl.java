fd5f51eda1eaf83aab959e64506414d80f1b05a2
Georgy Levin <g.phyzy@gmail.com>
2023-10-26 17:42:42 +0300

    HW 53 fixed + adds new implementation JoinStringStandardImpl

--- src/telran/performance/JoinStringsPerformanceAppl.java
+++ src/telran/performance/JoinStringsPerformanceAppl.java
@@ -1,67 +1,39 @@
 package telran.performance;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 import telran.strings.*;
 
 public class JoinStringsPerformanceAppl {
 
-	private static final String PAKAGE_STRINGS = "telran.strings.";
-
 	private static final int N_STRINGS = 1000;
 	private static final int N_RUNS = 1000;
-
+	private static final String BASE_PACKAGE = "telran.strings.";
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("must be not less one name of implementation");
-		}
-
-		try {
-			for (int i = 0; i < args.length; i++) {
-				String name = args[i];
-				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(PAKAGE_STRINGS + name);
+		String[] strings = getStrings();
+		for (String className: args) {
+			try {
+				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(BASE_PACKAGE + className);
 				Constructor<JoinStrings> constructor = clazz.getConstructor();
 				JoinStrings joinStrings = constructor.newInstance();
-				String testName = getTestName(name);
-				String[] strings = getStrings();
-
-				JoinStringsPerformanceTest testS = 
-						new JoinStringsPerformanceTest(testName, N_RUNS, strings, joinStrings);
-				testS.run();
+				JoinStringsPerformanceTest test =
+						new JoinStringsPerformanceTest(getTestName(clazz.getSimpleName()), N_RUNS, strings, joinStrings);
+				test.run();
+			} catch (ClassNotFoundException e) {
+				System.out.println(e.getMessage());
+			} catch(Exception e) {
+				e.printStackTrace();
+				
 			}
 			
-		} catch (ClassNotFoundException e) {
-			System.out.printf("name must be 'JoinStringsImpl' or 'JoinStringsBuilderImpl'");
-
-		} catch (NoSuchMethodException e) {
-			System.out.println("NoSuchMethodException " + e.getCause());
-
-		} catch (LinkageError e) {
-			System.out.println("the linkage fails " + e.getMessage());
 		}
 		
-		catch (InvocationTargetException e) {
-			System.out.println("InvocationTargetException" + e.getCause());
-
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-
-		}
-
 	}
 
 	private static String getTestName(String className) {
-
+		
 		return String.format("%s; Number of the strings is %d", className, N_STRINGS);
 	}
 
